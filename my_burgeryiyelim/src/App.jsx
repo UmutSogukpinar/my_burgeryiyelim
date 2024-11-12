6509416c10208ac074671aae0c7909f7dcc321b6
umutsogukpinar <usogukpinar0@gmail.com>
2024-11-12 10:15:59 +0300

    sketch of some component generated

--- my_burgeryiyelim/src/App.jsx
+++ my_burgeryiyelim/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AboutUs from "./aboutUs/AboutUs";
+import Home from "./Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div id="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ my_burgeryiyelim/src/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Navbar = () => {
+  return (
+    <div id='main-navbar'>Navbar</div>
+  )
+}
+
+export default Navbar
