d0170fb990e3ba27e7b6041c4e9ef4dca3c4447f
UmutSogukpinar <usogukpinar0@gmail.com>
2024-11-20 16:03:36 +0300

    about us page just started

--- my_burgeryiyelim/src/Components/aboutUs/AboutUs.jsx
+++ my_burgeryiyelim/src/Components/aboutUs/AboutUs.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Footer from '../footer/Footer'
+import "./aboutUs.css"
+import { LanguageContext } from '../../Contexts/LanguageContext'
 
 const AboutUs = () => {
+
+  const {brandName} = useContext(LanguageContext);
+
   return (
-    <div>AboutUs</div>
+    <div id='about-us'>
+      <main id='about-us-bg'>
+        
+        <section id='about-us-paraph'>
+          <h1>{brandName}</h1>
+        </section>
+      </main>
+      <Footer/>
+    </div>
   )
 }
 
